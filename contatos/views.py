f8322e6e4a4fe13f5e4b25d3fdc58a277b0c2a59
FulvioTakara <fulviojp@gmail.com>
2022-02-17 23:14:32 -0300

    pagina de contato individual

--- contatos/views.py
+++ contatos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Contato
 
 
@@ -6,4 +6,10 @@ def index(request):
     contatos = Contato.objects.all()
     return render(request, 'contatos/index.html', {
          'contatos': contatos
+    })
+
+def ver_contato(request, contato_id):
+    contato = get_object_or_404(Contato, id=contato_id)
+    return render(request, 'contatos/ver_contato.html', {
+         'contato': contato
     })
