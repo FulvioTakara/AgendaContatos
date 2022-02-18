85137f1dbeb5586ec3f9748e977a414bcea228ac
FulvioTakara <fulviojp@gmail.com>
2022-02-18 00:23:34 -0300

    paginação

--- contatos/admin.py
+++ contatos/admin.py
@@ -4,11 +4,12 @@ from .models import Categoria, Contato
 
 class ContatoAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email',
-                    'data_criacao', 'categoria')
+                    'data_criacao', 'categoria', 'mostrar')
     list_display_links = ('id', 'nome', 'sobrenome')
     #list_filter = ( 'categoria', 'nome', 'sobrenome')
     list_per_page = 10
     search_fields = ('categoria', 'id', 'nome', 'sobrenome', 'data_criacao')
+    list_editable = ('telefone', 'mostrar')
 
 admin.site.register(Categoria)
 admin.site.register(Contato, ContatoAdmin)
