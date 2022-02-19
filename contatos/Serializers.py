ced0b91cfacf7ed9bee7bb6ebaa82acec7339809
FulvioTakara <fulviojp@gmail.com>
2022-02-19 00:59:08 -0300

    Serializers

---
+++ contatos/Serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+
+from .models import Contato, Categoria
+
+
+class CategoriaSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Categoria
+        fields = ('nome',)
+
+
+class ContatoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        """
+        extra_kwargs = {
+            'email': {'write_only':True},
+            'telefone': {'write_only':True},
+        }
+        """
+
+        model = Contato
+        fields = (
+            'nome',
+            'sobrenome',
+            'telefone',
+            'email',
+            'descricao',
+            'mostrar',
+            'foto',
+            'categoria',
+        )
